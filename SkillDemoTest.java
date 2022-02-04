48d729d8f9a1a407721f8e1585bfd7035e6b2f6b
aliu104 <annieliu2002@gmail.com>
2022-02-04 14:37:05 -0800

    fixed test!

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void testSkillDemo() {
-        assertEquals("AnnieLiu", SkillDemo.formatName("Annie", "Liu")); // test should fail!
+        assertEquals("Annie Liu", SkillDemo.formatName("Annie", "Liu")); // test should fail!
     }
 }
